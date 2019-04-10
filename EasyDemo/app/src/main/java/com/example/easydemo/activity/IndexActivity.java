292f4d42d79396d5781a998f0c18594e9be60a40
Chaimouren <sxycwx2013@163.com>
2019-04-10 23:04:29 +0800

    add:添加启动页及跳转

---
+++ EasyDemo/app/src/main/java/com/example/easydemo/activity/IndexActivity.java
@@ -0,0 +1,16 @@
+package com.example.easydemo.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.easydemo.R;
+
+public class IndexActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+    }
+}
--- EasyDemo/app/src/main/java/com/example/easydemo/activity/SplashActivity.java
+++ EasyDemo/app/src/main/java/com/example/easydemo/activity/SplashActivity.java
@@ -2,10 +2,15 @@ package com.example.easydemo.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
 
 import com.example.easydemo.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * 默认启动界面
@@ -21,6 +26,14 @@ public class SplashActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_splash);
-
+        final Intent it = new Intent(this, IndexActivity.class); //你要转向的Activity
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(it); //执行
+            }
+        };
+        timer.schedule(task, 100 * 10); //10秒后*
     }
 }
